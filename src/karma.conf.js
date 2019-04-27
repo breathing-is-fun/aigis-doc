7a48df6cb7d9426212922ab6f7217200b5bf2f6b
orzorzorzorz <zy410419243@gmail.com>
2019-04-27 21:28:52 +0800

    ci: install headless chrome (#3)
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome
    
    * ci: install headless chrome

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function(config) {
   config.set({
     basePath: '',
@@ -25,9 +27,12 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 60000,
     customLaunchers: {
-      ChromeHeadlessCI: {
+      ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox'],
       },
