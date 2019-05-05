de4a5f64490067dce9e0f5ce7a742bc8fba8779c
zy410419243 <zy410419243@gmail.com>
2019-05-05 17:32:49 +0800

    test: update cases

--- src/service/meta-data.service.spec.ts
+++ src/service/meta-data.service.spec.ts
@@ -1,9 +1,13 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, async } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { MetaDataService } from './meta-data.service';
 
 describe('MetaDataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    }).compileComponents();
+  }));
 
   it('should be created', () => {
     const service: MetaDataService = TestBed.get(MetaDataService);
