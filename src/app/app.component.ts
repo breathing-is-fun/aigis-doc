27e62243ee504588c3bc2dd4592fb4724a0fceb1
zy410419243 <zy410419243@gmail.com>
2019-05-06 16:27:58 +0800

    fix: lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { MetaDataService } from '../service/meta-data.service';
 })
 export class AppComponent implements OnInit {
   dataSource: any = {};
-  title: string = '';
+  title = '';
 
   constructor(private metaDataService: MetaDataService) {}
 
