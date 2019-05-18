5e9bc516b68606c4e4374577cb0bcd8e0173c050
zy410419243 <zy410419243@gmail.com>
2019-05-18 08:44:24 +0800

    chore(prettier): replace node api with cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.metaDataService.getMapping().subscribe(mapping => {
-      this.metaDataService
-        .getData(mapping)
-        .subscribe(data => (this.dataSource = data));
+      this.metaDataService.getData(mapping).subscribe(data => (this.dataSource = data));
     });
   }
 }
